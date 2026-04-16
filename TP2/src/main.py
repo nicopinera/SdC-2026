19bedab243e2b224c9fd5a0d11aac24ef273bd28
Nicolas Piñera <nicolaspinera@gmail.com>
2026-04-16 17:35:00 -0300

    Modulo en python: Desacople del script en funciones

--- TP2/src/main.py
+++ TP2/src/main.py
@@ -1,42 +1,83 @@
-import requests,csv
+import requests,os,ctypes
+from dotenv import load_dotenv
+
+actual = os.path.abspath(__file__) # Ruta actual del script
+carpeta = os.path.dirname(actual) # Carpeta actual
+RUTA_BASE = os.path.dirname(carpeta) # Carpeta TP2
+RUTA_ENV = os.path.join(RUTA_BASE,".env") # Archivo .env
+RUTA_DATA = os.path.join(RUTA_BASE,"data")
+
+
+def cargar_env():
+    """
+    Funcion que carga las variables de entorno y devuelve
+    el url del banco mundial junto con el pais a analizar
+    """
+    load_dotenv(RUTA_ENV) # Carga de variables de entorno dentro del .env
+    return os.getenv('URL_BANCO_MUNDIAL'),os.getenv('PAIS') # Devolvemos la url del api
+
+def realizar_peticion_banco_mundial(url):
+    """
+    Realiza la peticion HTTP GET
+    """
+    resultado = requests.get(url)
+    if not resultado:
+        print("Error al realizar en HTTP GET")
+        return None
+    
+    if resultado.status_code != 200:
+        print(f"Fallo en peticion GET, status_code = {resultado.status_code}")
+        return None
+
+    datos = resultado.json()
+    return datos[1]
+
+def obtener_datos_pais(datos,pais):
+    """
+    Obtiene los años y valores del indice GINI de un pais especifico
+
+    Devuelve dos array: anios y valores
+    """
+    anios = []
+    valores = []
+    for item in datos:
+        anio = item['date'] # string
+        valor = item['value'] # float
+        pais_aux = item["country"]["value"]
+        if pais_aux == pais and valor is not None:
+            anios.append(anio)
+            valores.append(valor)
+    return anios,valores
+
+def generar_txt(pais,anios,valores):
+    txt = os.path.join(RUTA_DATA,f"{pais}.txt")
+    with open(txt,"w") as f:
+        for a,v in zip(anios,valores):
+            f.write(f"{a} - {v}\n")
+
+def c_y_asm(valores):
+    ruta_lib = os.path.join(RUTA_BASE,"libpython.so")
+    lib = ctypes.CDLL(ruta_lib)
+    
 
 def main():
-    pass
+    """
+    Funcion principal
+    """
 
-# Link API Banco mundial
-url_banco_mundial = ""
-resultado = requests.get(url=url_banco_mundial) # Se genera la peticion GET
-pais_buscado = "Argentina"
+    # Obtenemos las variables de entorno
+    url_banco_mundial,pais = cargar_env()
 
-print("---"*10)
-if resultado:
-    print("Se obtuno resultado con metodo GET")
-else:
-    print("Error al obtener informacion")
+    # Obtenemos la informacion por HTTP GET
+    informacion = realizar_peticion_banco_mundial(url=url_banco_mundial)
+    if not informacion:
+        print("Error al obtener informacion")
 
-print("Status Code: ", resultado.status_code)
-print("---"*10)
-print(f"Procesando datos de API para {pais_buscado.upper()}")
+    anios, valores = obtener_datos_pais(informacion,pais)
+    
+    generar_txt(pais,anios,valores)
 
-# Se convierte los datos a JSON
-data = resultado.json()
-registros = data[1] # Utilizamos el segund item que tiene los datos
 
-# Abrimos el csv
-with open('datos_gini.csv','w',newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['anio', 'valor']) # Encabezados
-
-    for item in registros:
-        anio = item['date']
-        valor = item['value']
-        pais = item["country"]["value"]
-        if pais == pais_buscado and valor is not None:
-            print(f"Año {anio} : {valor}")
-            writer.writerow([anio,valor])
-
-print("---"*10)
-print("Fin de programa")
 
 if __name__ == "__main__":
     main()
