bc9d54b0a1706be07fd01198bb4caac78eb673fb
Julian Krede <kredejulian@gmail.com>
2026-04-17 19:24:53 -0300

    Agrega explicacion de la primera parte del tp

--- TP2/src/func_asm.asm
+++ TP2/src/func_asm.asm
@@ -3,7 +3,7 @@ global cast
 
 cast:
     ; x viene en xmm0 (porque es float)
-    cvttss2si eax, xmm0   ; float → int (truncado)
+    cvttss2si eax, xmm0   ; float a int (truncado)
     add eax, 1            ; +1
 
     ret
