5b7798b94b67aca303b71f7bf4e68ed80a25a004
Julian Krede <kredejulian@gmail.com>
2026-04-17 14:07:39 -0300

    Corrige para leer el txt y castear el valor del indice

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cast(float x) {
-  int val = (int)x;
-  return val + 1;
+int castear_datos(float x) {
+
+  int val = (int)x + 1;
+  return val;
 }
 
-int main(void) {
-  int a = cast(7.4);
-  printf("%d", a);
+int main() {
+  FILE *file = fopen("data/Argentina.txt", "r");
+  if (file == NULL) {
+    perror("Error al abrir el archivo");
+    return 1;
+  }
 
+  int anio;
+  float gini;
+
+  while (fscanf(file, "%d - %f", &anio, &gini) == 2) {
+    int resultado = castear_datos(gini);
+    printf("Año: %d | Gini original: %.2f | Procesado: %d\n", anio, gini,
+           resultado);
+  }
+
+  fclose(file);
   return EXIT_SUCCESS;
 }
